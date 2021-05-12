be733dcc4098a8132201896cf138d675b21d03cd
Daria Vilkova <d.vilkova@postgrespro.ru>
2021-05-12 10:06:06 +0300

    fix: handle case where user has no nickname

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ import logging
 from typing import Dict
 
 import telegram
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler,Filters, MessageHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Filters, MessageHandler
 from telegram.ext.dispatcher import run_async
 from bot.reply import reply_msg, judge_msg, prob_markup
 from bot.user import User
 from bot.config import TOKEN
 from bot import backend
-from random import randrange
 
 request = telegram.utils.request.Request(con_pool_size=20)
 bot = telegram.Bot(token=TOKEN, request=request)
@@ -21,7 +20,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 ENTITY: Dict[str, User] = {}
 
-def send_new_problem(user_id,chat_id):
+
+def send_new_problem(user_id, chat_id):
     global ENTITY
     uid = str(user_id)
     prob = ENTITY[uid].get_problem()
@@ -41,6 +41,7 @@ def send_new_problem(user_id,chat_id):
                  If you want to know your score type /status"""
         )
 
+
 def send_new_problem_with_photo(user_id, chat_id):
     global ENTITY
     uid = str(user_id)
@@ -52,14 +53,14 @@ def send_new_problem_with_photo(user_id, chat_id):
             parse_mode='HTML',
             text=prob.text(),
         )
-        #photo = photos[randrange(len(photos))]
+        # photo = photos[randrange(len(photos))]
         with open(prob.photo_name, 'rb') as f:
             bot.send_photo(
                 chat_id=chat_id,
                 parse_mode='HTML',
                 photo=f,
                 reply_markup=prob_markup(prob.quiz_uuid, hint=prob.hint)
-        )
+            )
     else:
         bot.send_message(chat_id=chat_id, text=reply_msg('finish'))
         bot.send_message(
@@ -71,14 +72,20 @@ def send_new_problem_with_photo(user_id, chat_id):
 
 
 def company_name_handler(update, _):
-    #update.message.reply_text(update.message.text)
-    #message_text = update.message.text
+    # update.message.reply_text(update.message.text)
+    # message_text = update.message.text
     global ENTITY
 
     chat_id = update.message.chat_id
     user_id = update.message.chat.id
     uid = str(user_id)
-    nickname = update.message.from_user.username
+
+    if update.message.from_user.username is not None:
+        nickname = update.message.from_user.username
+    else:
+        nickname = update.message.from_user.first_name
+        if update.message.from_user.last_name is not None:
+            nickname += ' ' + update.message.from_user.last_name
 
     user = User(nickname, uid)
 
@@ -103,7 +110,7 @@ def start_handler(update, _):
     chat_id = update.message.chat_id
     user_id = update.message.chat.id
     uid = str(user_id)
-    #nickname = update.message.from_user.username
+    # nickname = update.message.from_user.username
 
     if uid in ENTITY:
         send_new_problem_with_photo(user_id, chat_id)
@@ -118,13 +125,13 @@ def start_handler(update, _):
         text="""Please enter your company name """
     )
 
+
 def callback_handler(update, _):
     global ENTITY
 
     ans, quiz_uuid = update.callback_query.data.split(' ')
     msg = update.callback_query.message
     user_id = update.callback_query.message.chat.id
-
 
     uid = str(user_id)
     if uid not in ENTITY:
@@ -154,6 +161,7 @@ def callback_handler(update, _):
         bot.send_message(chat_id=msg.chat_id, text=judge_msg(result))
 
         send_new_problem_with_photo(user_id, msg.chat_id)
+
 
 def status_handler(update, _):
     global ENTITY
@@ -188,8 +196,10 @@ def feedback_handler(update, _):
         text=reply
     )
 
+
 def error_handler(update, context):
     logger.error('Update "%s" caused error "%s"', update, context.error)
+
 
 def main():
     updater = Updater(token=TOKEN, use_context=True)
@@ -204,6 +214,7 @@ def main():
 
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
